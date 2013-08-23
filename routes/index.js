917df274bdf18de89c9f2c2ae12f3881ad6a0545
Alevardi <Amet.Alvirde@gmail.com>
2013-08-23 09:02:11 -0500

    basic structure

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Paraíso' });
 };
+
+exports.short_name = function(req, res){
+  res.render('short_name', { title: 'Paraíso' });
+};
+
+exports.talent_management = function(req, res){
+  res.render('organizational_structure/talent_management', { title: 'Paraíso' });
+};
+
