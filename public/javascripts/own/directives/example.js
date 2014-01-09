80723d3cabe4fab134b4956a9af3b06c381fd7d4
Enrique <enrique199413@gmail.com>
2014-01-08 23:20:46 -0600

    Check the view and select the best option for validations

--- public/javascripts/own/directives/example.js
+++ public/javascripts/own/directives/example.js
@@ -2,28 +2,41 @@ var ejemplo = angular.module( 'Validation', [] );
 ejemplo.directive('ngValidation' , function(){
   return{
     restrict: 'A',
-    template: '<div class="label label-danger pull-right"><small>{{field}}</small></div>',
-    require: '^?form',
-    scope:{field:'='},
-    controller: ['$scope', function( $scope ) {
+    
+    require: 'ngModel',
+    scope:{field:'@'},
+    /*controller: ['$scope', function( $scope ) {
       $scope.funcion = function(Datos){
+        
         console.log(Datos);
       };
-    }],
+    }],*/
     link: function (scope, iElement, iAttrs, ctrl){
-      scope.funcion(iAttrs.field);
-      scope.$watch('field',function(data){
+      /*scope.$watch('field',function(data){
+        /*scope.funcion(iAttrs.field);
         console.log(data);
         console.log("Data");
         console.log(iAttrs);
         console.log("iAttrs");
         console.log(iElement);
-        console.log("iElement");
-        console.log(ctrl);
-        console.log("ctrl");
-        
-        console.log(hola);
-      });
-    }
+        console.log("iElement");*/
+        var regex = /^\d$/;
+        var validator = function(value){
+        //var floatValue = parseFloat(value);
+          ctrl.$setValidity('validNumber', regex.test(value));
+          console.log(scope.form.$setPristine());
+          return value;
+        };
+        ctrl.$parsers.unshift(validator);
+        ctrl.$formatters.unshift(validator);
+      }
+        /*console.log(ctrl['$valid']);
+        if ( scope.Validate )
+        ctrl['$valid'] = false;
+        console.log(ctrl['$valid']);
+
+
+      });*/
+    
   }
 });
