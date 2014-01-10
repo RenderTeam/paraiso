c24eea821527808c4d25a67b541343934c651aae
Enrique <enrique@enrique-Satellite-L635.(none)>
2014-01-10 05:03:01 -0600

    Directive Validation success but check the files and source code

--- public/javascripts/own/directives/example.js
+++ public/javascripts/own/directives/example.js
@@ -1,54 +1,48 @@
-var ejemplo = angular.module( 'Validation', [] );
-ejemplo.directive('ngValidation' , function(){
-  return{
-    restrict: 'A',
-    require: 'ngModel',
-    scope:{validationtype:'@'},
-    /*controller: ['$scope', function( $scope ) {
-      $scope.funcion = function(Datos){
-        
-        console.log(Datos);
-      };
-    }],*/
-    link: function (scope, iElement, iAttrs, ctrl){
-            if( ctrl ){
-              ctrl.$parsers = [];
-              ctrl.$formatters = [];
-              var regex = /^\d$/;
-              var val = iAttrs.valuefield;
-              if(isEmpty(val)){
-                val = 'Error';
-              }
-              var validateField = function ( value ){
-                console.log("Validating as email", value);
-                if (isEmpty(value)) {
-                  iElement.after('<div id=hola class=\'label label-danger pull-right\''+ 
-                '><small>'+ val +'</small></div>');
-                  ctrl.$setValidity('required', false);
-                } else {
-                  iElement.remove(":contains('hola')");
-                  console.log(iElement.children('div').remove());
-                  ctrl.$setValidity('required', true);
+var validation = angular.module( 'Validation', [] );
+validation.directive( 'rdValidation' , function() {
+    return {
+      require: 'ngModel',
+      link: function($scope, $element, $attrs, ngModelController) {
+        var val = $attrs.vtext;
+        if(isEmpty(val)){
+          val = 'Error';
+        }
+        ngModelController.$formatters.push(function(value) {
+          ngModelController.$setValidity('required', true);
+          return value;
+        });
+        $element.after('<div id=\'elementor'+$attrs.ngModel.replace('.','')+'\' class=\'label label-danger pull-right\' hidden>'+val+'</h1>');
+        $('#elementor'+$attrs.ngModel.replace('.','')+'').hide();
+        ngModelController.$parsers.push(function(value) {
+          if (validationstypes($attrs.validationtype, value)){
+            ngModelController.$setValidity('required', true);
+            $('#elementor'+$attrs.ngModel.replace('.','')+'').fadeOut();
+          }else{
+            $('#elementor'+$attrs.ngModel.replace('.','')+'').fadeIn();
+          }
+          console.log('Despues', true);
+          return true ? value : undefined;
+        });
+        function isEmpty(value) {
+          return value === '' || value === undefined || value === null ||
+            value === false || value !== value;
+        }
+        function validationstypes( forvalidate, val ){
+          if(isEmpty(val)){
+            return false;
+          }else{
+            switch(forvalidate){
+              case 'text':
+                var pattern = /^[a-zA-Z]*$/;
+                if(val.match(pattern)){
+                  return true;
+                }else{
+                  return false;
                 }
-              };
-              /*console.log(ctrl['viewValue'].$isEmpty());
-              if( !viewValue.length > 0 ){
-                viewValue = 'Error';
-              }*/
-              
-              /*var validator = function(value){
-              //var floatValue = parseFloat(value);
-                ctrl.$setValidity('validNumber', regex.test(value));
-                //console.log(scope.form.$setPristine());
-                return value;
-              };*/
-              function isEmpty(value) {
-                return value === '' || value === undefined || value === null ||
-                  value === false || value !== value;
+              break;
               }
-              ctrl.$parsers.unshift(validateField);
-              ctrl.$formatters.unshift(validateField);
-            }
-    }
-  }
-});
+          }
+        }
+      }
+    };
+  });
