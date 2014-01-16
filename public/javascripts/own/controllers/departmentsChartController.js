b2a77205b9fd033b79df433fa9e9382c7cb5c594
ardroz <darr.0725@gmail.com>
2014-01-16 15:04:26 -0600

    Departments chart finished needs to be refactored

--- public/javascripts/own/controllers/departmentsChartController.js
+++ public/javascripts/own/controllers/departmentsChartController.js
@@ -1,85 +1,19 @@
 function departmentsChartController ( scope, departments, employment, employmentTree ) {
   var chart = { 
     name: '',
-    children: []
+    children: [],
+    size: 1
   };
+
   departments.getDepartments().
     success( function ( data ) {
       data.forEach( function ( element, index, array ) {
         if ( index + 1 < array.length ) {
           createDepartment( element.name, chart );
         } else {
-          createDepartment( element.name, chart, function ( finalChart ) {
-            console.log( finalChart );
-
-            var w = 1100,
-                h = 700,
-                r = 500,
-                x = d3.scale.linear().range([0, r]),
-                y = d3.scale.linear().range([0, r]),
-                node,
-                root;
-
-            var pack = d3.layout.pack()
-                .size([r, r])
-                .value(function(d) { return d.size; })
-
-            var vis = d3.select("#chart").insert("svg:svg", "h2")
-                .attr("width", w)
-                .attr("height", h)
-              .append("svg:g")
-                .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
-
-            d3.json("/json/area.json", function(data) {
-              node = root = finalChart;
-              console.log(finalChart);
-
-              var nodes = pack.nodes(finalChart);
-
-              vis.selectAll("circle")
-                  .data(nodes)
-                .enter().append("svg:circle")
-                  .attr("class", function(d) { return d.children ? "parent" : "child"; })
-                  .attr("cx", function(d) { return d.x; })
-                  .attr("cy", function(d) { return d.y; })
-                  .attr("r", function(d) { return d.r; })
-                  .on("click", function(d) { return zoom(node == d ? root : d); });
-
-              vis.selectAll("text")
-                  .data(nodes)
-                .enter().append("svg:text")
-                  .attr("class", function(d) { return d.children ? "parent" : "child"; })
-                  .attr("x", function(d) { return d.x; })
-                  .attr("y", function(d) { return d.y; })
-                  .attr("dy", ".35em")
-                  .attr("text-anchor", "middle")
-                  .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
-                  .text(function(d) { return d.name; });
-
-              d3.select(window).on("click", function() { zoom(root); });
-            });
-
-            function zoom(d, i) {
-              var k = r / d.r / 2;
-              x.domain([d.x - d.r, d.x + d.r]);
-              y.domain([d.y - d.r, d.y + d.r]);
-
-              var t = vis.transition()
-                  .duration(d3.event.altKey ? 7500 : 750);
-
-              t.selectAll("circle")
-                  .attr("cx", function(d) { return x(d.x); })
-                  .attr("cy", function(d) { return y(d.y); })
-                  .attr("r", function(d) { return k * d.r; });
-
-              t.selectAll("text")
-                  .attr("x", function(d) { return x(d.x); })
-                  .attr("y", function(d) { return y(d.y); })
-                  .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
-
-              node = d;
-              d3.event.stopPropagation();
-            }
+          var lol = createDepartment( element.name, chart, function ( lol ) {
+            console.log(lol);
+            chartConstructor( lol )
           });
         }
       } );
@@ -87,20 +21,21 @@ function departmentsChartController ( scope, departments, employment, employment
     } ).
     error();
 
-  
-
   function createDepartment ( department, chart, cb ) {
+    
     employment.getEmploymentsByDepartment( { department: department }).
       success( function ( employments ) {
         if ( employments.length < 1 ){
-          chart.children.push( { name: department, children: [] } );
+          chart.children.push( { name: department, children: [], size: 1 } );
         } else {
           var highest = getHighestEmployment( employments );
           employmentTree.getSmallEmploymentsTree( { employment: highest} ).
-            success( function ( data ) {
-              chart.children.push( { name: department, children: data } );
-              if ( cb !== undefined ) {
-                cb( chart );
+            success( function ( smallTree ) {
+              var children = [];
+              children.push(smallTree);
+              chart.children.push( { name: department, children: children, size: 1 } );
+              if (cb !== undefined) {
+                cb ( chart );
               }
             } ).
             error();
@@ -120,5 +55,125 @@ function departmentsChartController ( scope, departments, employment, employment
     } );
 
     return highest;
+  }
+}
+
+function chartConstructor ( hommie ) {
+  var w = 1100,
+      h = 800,
+      r = 600,
+      x = d3.scale.linear().range([0, r]),
+      y = d3.scale.linear().range([0, r]),
+      node,
+      root;
+
+  var pack = d3.layout.pack()
+      .size([r, r])
+      .value(function(d) { return d.size; });
+
+  var vis = d3.select("#chart").insert("svg:svg", "h2")
+      .attr("width", w)
+      .attr("height", h)
+    .append("svg:g")
+      .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
+
+  node = root = hommie;
+  var nodes = pack.nodes(hommie);
+
+  vis.selectAll("circle")
+      .data(nodes)
+    .enter().append("svg:circle")
+      .attr("class", function(d) {  })
+      .attr("class", function ( d ) {
+        if ( d.hasOwnProperty( "parent" ) ) {
+          if ( !d.parent.hasOwnProperty("parent") ){
+            return "department";
+          } else {
+            return d.children ? "parent" : "child";
+          }
+        } else {
+          return d.children ? "parent" : "child";
+        }
+      })
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", calculateRadius )
+      .on("click", function(d) { return zoom(node == d ? root : d); });
+
+  vis.selectAll("text")
+      .data(nodes)
+    .enter().append("svg:text")
+      .attr("class", function ( d ) {
+        if ( d.hasOwnProperty( "parent" ) ) {
+          if ( !d.parent.hasOwnProperty("parent") ){
+            return "department";
+          } else {
+            return d.children ? "parent" : "child";
+          }
+        } else {
+          return d.children ? "parent" : "child";
+        }
+      })
+      .attr("x", function(d) { return d.x; })
+      .attr("y", textYAlign)
+      .attr("dy", ".35em")
+      .attr("text-anchor", "middle")
+      .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
+      .text(function(d) { return d.name; });
+
+
+  d3.select(window).on("click", function() { zoom(root); });
+
+  function zoom(d, i) {
+    var k = r / d.r / 2;
+    x.domain([d.x - d.r, d.x + d.r]);
+    y.domain([d.y - d.r, d.y + d.r]);
+
+    var t = vis.transition()
+        .duration(d3.event.altKey ? 7500 : 750);
+
+    t.selectAll("circle")
+      .attr("cx", function(d) { return x(d.x); })
+      .attr("cy", function(d) { return y(d.y); })
+      .attr("r", function( d ) { 
+        if ( d.hasOwnProperty( "parent" ) ) {
+          return d.parent.children.length === 1 ? k * d.parent.r - 10: k * d.r;
+        } else {
+          return k* d.r; 
+        }
+      });
+
+    t.selectAll("text")
+      .attr("x", function(d) { return x(d.x); })
+      .attr("y", function(d) { return y(d.y - d.r / 4); })
+      .attr("y", function(d) { 
+        var number;
+        if ( d.hasOwnProperty( "children" ) ){
+            number = d.children.length === 1 ? d.y - d.r / 2:d.y - d.r / 4;
+          } else {
+            number = d.y - d.r / 4;
+          }
+        return y(number); 
+      })
+      .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+
+    node = d;
+    d3.event.stopPropagation();
+  }
+}
+
+function calculateRadius ( d ) { 
+  if ( d.hasOwnProperty( "parent" ) ) {
+    return d.parent.children.length === 1 ? d.parent.r - 10: d.r;
+  } else {
+    return d.r; 
+  }
+}
+
+function textYAlign ( d ) { 
+  if ( d.hasOwnProperty( "children" ) ){
+    return d.children.length === 1 ? d.y - d.r / 2:d.y - d.r / 4; 
+  } else {
+    return d.y - d.r / 4;
   }
 }
